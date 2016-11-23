00afa701727aad9bb098de418d81215be14b5016
Sonia Wu <swu52@hawk.iit.edu>
2016-11-23 12:08:28 -0600

    Add self invoking function, map marker

--- js/zipPlaces.js
+++ js/zipPlaces.js
@@ -2,23 +2,38 @@ $.noConflict();
 (function($) {
   $(document).ready(
     function() {
+     // console.log('I am here');
        $('#cityZip-form').on('submit', function(event) {
-         console.log('I am here');
+        event.preventDefault();
+       // console.log('got submitted'); 
         var zip = $('#zip').val();
         var url = 'http://api.zippopotam.us/us/' + zip;
         $.get(url,
           function(data,json) {
-        
+                  
+          if (data != null) {
           var city = data.places[0]['place name'];
-          var country = data.country;
+          var state = data.places[0].state;
           var longitude = data.places[0].longitude; 
           var latitue = data.places[0].latitude; 
+          var latLng = new google.maps.LatLng(latitue, longitude);
           var mapOptions = {
-            center: new google.maps.LatLng(latitue, longitude),
-            zoom: 12,
+            center: latLng,
+            zoom: 10,
           };
-          $('#cityZip').append('<p>The city is '+city+' lat : '+ latitue+' lng : '+longitude+'</p>');
+          
+          var infoStr = 'The city is ' + city + ' in ' + state + ' state';
+          $('#cityInfo').text(infoStr);
+  
           var map = new google.maps.Map(document.getElementById('map'), mapOptions); 
+          var marker = new google.maps.Marker({
+            position: latLng,
+            map:map
+          });
+          } else {
+            // This part of code is not yet working
+            console.log('Invalid zip code');
+          } 
           }
         )
       })
